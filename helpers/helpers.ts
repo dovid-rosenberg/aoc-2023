5ceba3121ece7f39efa60b83942eac6d71ca97c9
dovid rosenberg <dovidrosenberg@outlook.com>
2023-12-03 21:34:39 -0500

    ability to get subset of data for testing

--- helpers/helpers.ts
+++ helpers/helpers.ts
@@ -1,10 +1,11 @@
 import * as fs from "node:fs/promises";
 import * as path from "node:path";
 
-export async function parseInput({rootDir, fileName = 'input.txt'}){
+export async function parseInput({rootDir, fileName = 'input.txt',testQuantity}){
     const rawData = await  fs.readFile(path.join(rootDir, fileName), { encoding: 'utf8' })
-    const array = rawData.split("\n")
-//    array.splice(-1,1)
-    
+    let array = rawData.split("\n")
+    if(testQuantity){
+        array = array.splice(0,testQuantity)
+    }
     return array
 }
